e62b78f118c14d0fc107ac6bacf09b4862942d32
Nichole Bates <nbates88@gmail.com>
2016-08-01 16:45:22 -0400

    flux dispatcher in pla in place

--- src/js/stores/TodoStore.js
+++ src/js/stores/TodoStore.js
@@ -1,5 +1,7 @@
 import { EventEmitter } from 'events';
 
+import dispatcher from '../dispatcher';
+
 class TodoStore extends EventEmitter {
 	constructor() {
 		super();
@@ -31,8 +33,17 @@ class TodoStore extends EventEmitter {
 	getAll() {
 		return this.todos;
 	}
+
+	handleActions(action) {
+		switch(action.type) {
+			case "CREATE_TODO": {
+				this.createTodo(action.text);
+			}
+		}
+	}
 }
 
 const todoStore = new TodoStore;
-window.todoStore = todoStore;
+dispatcher.register(todoStore.handleActions.bind(todoStore))
+window.dispatcher = dispatcher;
 export default todoStore;

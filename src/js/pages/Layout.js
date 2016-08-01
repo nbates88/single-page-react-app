0643a87b8540a9cd4b1b9e111ef3760096ffecf1
Nichole Bates <nbates88@gmail.com>
2016-08-01 12:37:46 -0400

    added some links and archives state

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -6,9 +6,11 @@ import Nav from "../components/layout/Nav";
 
 export default class Layout extends React.Component {
   render() {
-  
+      <div>
+      <h1>KillerNews.net</h1>
+        {this.props.children}
+        <Link to='archives'>archives</Link>
+        <Link to='settings'>settings</Link>
       </div>
-
-    );
   }
 }

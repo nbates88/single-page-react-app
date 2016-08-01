970ff279189eda0f2495b6fc3d48473491afd10b
Nichole Bates <nbates88@gmail.com>
2016-08-01 17:12:00 -0400

    fix memory leak from actions

--- src/js/pages/Todos.js
+++ src/js/pages/Todos.js
@@ -6,18 +6,25 @@ import TodoStore from "../stores/TodoStore";
 export default class Featured extends React.Component {
   constructor() {
     super();
+    this.getTodos = this.getTodos.bind(this);    
     this.state = {
       todos: TodoStore.getAll()
      }; 
   }
 
    componentWillMount() {
-      TodoStore.on('change', () => {
-        this.setState({
-          todos: TodoStore.getAll(),
-        });
-      });
+      TodoStore.on('change', this.getTodos);
   } 
+
+  componentWillUnmount() {
+    TodoStore.removeListener('change', this.getTodos);
+  }
+
+  getTodos() {
+    this.setState({
+      todos: TodoStore.getAll(),
+    })
+  }
 
   reloadTodos() {
     TodoActions.reloadTodos(Date.now());
